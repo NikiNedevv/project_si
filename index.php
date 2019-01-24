dc0a58bcbed4213990987882a7a2b1d5326b89ec
NikiNedevv <45046194+NikiNedevv@users.noreply.github.com>
2019-01-24 18:15:59 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>index</title>
+	<?php echo '<p>Всички карти и техните стойности. </p>';
+	echo		"'KC' => 13, 'QC' => 12, 'JC' => 11, '10C' => 10, '9C' => 9, '8C' => 8, '7C' => 7, '6C' => 6, '5C' => 5, '4C' => 4, '3C' => 3, '2C' => 2, 'AC' => 1,
+			'KH' => 13, 'QH' => 12, 'JH' => 11, '10H' => 10, '9H' => 9, '8H' => 8, '7H' => 7, '6H' => 6, '5H' => 5, '4H' => 4, '3H' => 3, '2H' => 2, 'AH' => 1,
+			'KS' => 13, 'QS' => 12, 'JS' => 11, '10S' => 10, '9S' => 9, '8S' => 8, '7S' => 7, '6S' => 6, '5S' => 5, '4S' => 4, '3S' => 3, '2S' => 2, 'AS' => 1,
+			'KD' => 13, 'QD' => 12, 'JD' => 11, '10D' => 10, '9D' => 9, '8D' => 8, '7D' => 7, '6D' => 6, '5D' => 5, '4D' => 4, '3D' => 3, '2D' => 2, 'AD' => 1," ;
+			?>
+<form method="get" action="proekt.php">
+	<input type="text" name="cards">
+	<input type="submit" name="submit">
+</form>
+</head>
+<body>
+	
+</body>
+</html>
+
