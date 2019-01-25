e34ab8bfbe17f3dfc43bb2e35bbeb2800213adb4
NikiNedevv <45046194+NikiNedevv@users.noreply.github.com>
2019-01-26 00:27:13 +0200

    Add files via upload

--- proekt.php
+++ proekt.php
@@ -1,17 +1,31 @@
 <?php
-if (!empty($str)) {
-	$str = $_GET['cards'];
+if (!empty($_GET['cards'])) {
+	
 
 
-$x=explode(" ",$str); //експлоудваме стринга
+$cards = explode(" ",$_GET['cards']); //експлоудваме стринга
 
-    $s=["C","H","S","D"];
+    $suits=["C","H","S","D"];
 
-    $n=["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+    $values=["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
 
-    $a = array_search(substr($x[0],-1),$s); //Търсим съвпадения в "боите" на картите
-    
-    $b = array_search(substr($x[0],0,-1),$n); //Търсим съвпадения в стойностите
+    $str_suit = array_search(substr($cards[0],-1),$suits); //Търсим съвпадения в "боите" на картите
+
+    $str_value = array_search(substr($cards[0],0,-1),$values); //Търсим съвпадения в стойностите
     $si_steb_count = 1;
 
+foreach ($cards as  $value) {
+	if ($value == $values[$str_value].$suits[$str_suit]) {
+            $str_suit = ($str_suit + 1) % 4;
+            $str_value = ($str_value + 3) % 13;
+            ++$si_steb_count;
+        } else {
+            echo 'ends on' . $si_steb_count;
+        }
+    }
+    echo 1;
 }
+
+
+
+
