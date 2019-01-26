f2ba257b80c230f79a79559e15f518ba7579d297
NikiNedevv <45046194+NikiNedevv@users.noreply.github.com>
2019-01-26 22:51:32 +0200

    polished

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>index</title>
+	<link rel="stylesheet" href="./style.css">
+</head>
+<body>
+	 <center><p>Всички карти. </p></center>
+	<center><p>Clubs		 KC QC JC 10C 9C 8C 7C 6C 5C 4C 3C 2C AC</p></center>
+	<center><p>Hearths	 	 KH QH JH 10H 9H 8H 7H 6H 5H 4H 3H 2H AH</p></center>
+	<center><p>Spades	 	 KS QS JS 10S 9S 8S 7S 6S 5S 4S 3S 2S AS</p></center>
+	<center><p>Diamonds	 	 KD QD JD 10D 9D 8D 7D 6D 5D 4D 3D 2D AD</p></center>
+			
+<form method="get" action="proekt.php" id="form">
+	<center><input type="text" name="cards" id="cards"></center>
+	<center><input type="submit" name="submit" id = "submit"></center>
+</form>
+</body>
+</html>
+
